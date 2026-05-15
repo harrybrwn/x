063167019481916c40bc684cd2a7e7fc7147c850
Harry Brown <me@h3y.sh>
2026-05-15 09:12:26 -0700

    cobrautil: update shared help template

--- cobrautil/template.go
+++ cobrautil/template.go
@@ -1,3 +1,4 @@
+// Package cobrautil holds shared code for working with spf13/cobra.
 package cobrautil
 
 import (
@@ -16,35 +17,43 @@ func init() {
 	})
 }
 
-// This is a template for cobra commands that more
-// closely imitates the style of the go command help
-// message.
-var IndentedCobraHelpTemplate = `Usage:{{if .Runnable}}
+// IndentedCobraUsageTemplate is a template for cobra commands that more closely
+// imitates the style of the go command help message.
+var IndentedCobraUsageTemplate = `Usage:{{if .Runnable}}
 
 	{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+
 	{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
 
 Aliases:
+
 	{{.NameAndAliases}}{{end}}{{if .HasExample}}
 
 Examples:
-	{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}{{$cmds := .Commands}}{{if eq (len .Groups) 0}}
 
 Available Commands:
-{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-	{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+{{range $cmds}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+	{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{else}}{{range $group := .Groups}}
+
+{{.Title}}
+{{range $cmds}}{{if (and (eq .GroupID $group.ID) (or .IsAvailableCommand (eq .Name "help")))}}
+	{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if not .AllChildCommandsHaveGroup}}
+
+Additional Commands:
+{{range $cmds}}{{if (and (eq .GroupID "") (or .IsAvailableCommand (eq .Name "help")))}}
+	{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
 
 Flags:
 
-{{.LocalFlags.FlagUsagesWrapped 100 | trimTrailingWhitespaces | indent}}{{end}}{{if .HasAvailableInheritedFlags}}
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces | indent}}{{end}}{{if .HasAvailableInheritedFlags}}
 
 Global Flags:
 
 {{.InheritedFlags.FlagUsages | trimTrailingWhitespaces | indent}}{{end}}{{if .HasHelpSubCommands}}
 
-Additional help topics:
-{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
-	{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+	{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
 
 Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
 `
